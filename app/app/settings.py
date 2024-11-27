f9bfa50fa27b000724751fad544b3658b5a6fa91
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-27 21:21:46 +0100

    Fixed database settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -65,9 +65,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'EGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
     }
