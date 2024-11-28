a62e60d5ffe92008dbc18987e3280ce8b5be4aa5
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-28 21:54:27 +0100

    Add test for user email exists

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -36,3 +36,15 @@ class PublicUserApiTesrs(TestCase):
         user = get_user_model().objects.get(email=payload.email)
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
+
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name'
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
